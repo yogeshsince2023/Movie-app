0c4958e1e2c5c5f6245375bc546b4decb37f6b5c
yogeshsince2023 <yogeshtaparia82@gmail.com>
2026-01-02 00:47:28 +0530

    Fix base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/Movie_app/',
+  base: '/Movie-app/',
 })
